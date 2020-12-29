85826b405c52f5bc27b3aa720d8a5df8c096a4a5
Aycan Deniz Vit <avit16@ku.edu.tr>
2020-12-30 01:06:58 +0300

    Add publisher

--- spring-webapp/src/main/java/com/avit/webapp/bootstrap/BootStrapData.java
+++ spring-webapp/src/main/java/com/avit/webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.avit.webapp.bootstrap;
 
 import com.avit.webapp.domain.Author;
 import com.avit.webapp.domain.Book;
+import com.avit.webapp.domain.Publisher;
 import com.avit.webapp.repositories.AuthorRepository;
 import com.avit.webapp.repositories.BookRepository;
+import com.avit.webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,26 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher publisher = new Publisher();
+        publisher.setName("Pearson");
+        publisher.setCity("Los Angeles");
+        publisher.setState("CA");
+
+        publisherRepository.save(publisher);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
         Author aycan = new Author("Aycan", "Vit");
         Book book = new Book("Domain Driven Design", "123123");
         aycan.getBooks().add(book);
